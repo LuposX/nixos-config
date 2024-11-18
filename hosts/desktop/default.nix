1b468496e75a6a49af67520a296eca90e8cccddc
installer <installer@gmail.com>
2024-11-18 12:02:44 +0100

    grub

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -9,6 +9,7 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "nodev";
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.efiSupport = true;
   boot.loader.timeout = 5;
   boot.loader.grub.useOSProber = true;
 
