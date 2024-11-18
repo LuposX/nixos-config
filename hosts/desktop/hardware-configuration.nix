d13f6343ae6634d016ad7a7ee63f6dd58d15f347
installer <installer@gmail.com>
2024-11-18 12:41:26 +0100

    idk

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -22,6 +22,7 @@
     [ { device = "/dev/disk/by-uuid/8805928e-f49d-4afa-9559-197622e50abd"; }
     ];
 
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
