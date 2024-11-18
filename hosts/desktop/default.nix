6e2cfc3337f71c831ecb5a1b9129b450e1b72ead
installer <installer@gmail.com>
2024-11-18 12:47:21 +0100

    boot

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -6,13 +6,14 @@
   ];
 
   # Boot
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.timeout = 5;
-  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = true;
+  # For Grub
+  #boot.loader.grub.enable = true;
+  #boot.loader.grub.device = "nodev";
+  #boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.grub.efiSupport = true;
+  #boot.loader.timeout = 5;
+  #boot.loader.grub.useOSProber = true;
 
   # Extra kernel Parameter are for silent boot
   boot.kernelParams = ["quiet" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "vt.global_cursor_default=0"];
