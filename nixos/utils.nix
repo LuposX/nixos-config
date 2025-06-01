6376fd22fd4eaa2400d8c1fa5b2353088aac2fb7
LuposX <schuppsimon5@gmail.com>
2025-06-01 11:24:46 +0200

    modularized configuration.nix further
    further utils

---
+++ nixos/utils.nix
@@ -0,0 +1,129 @@
+{ pkgs, config, inputs, ... }:
+let
+  hostname = config.var.hostname;
+  keyboardLayout = config.var.keyboardLayout;
+  configDir = config.var.configDirectory;
+  timeZone = config.var.timeZone;
+  defaultLocale = config.var.defaultLocale;
+  extraLocale = config.var.extraLocale;
+  autoUpgrade = config.var.autoUpgrade;
+in {
+  networking.hostName = hostname;
+
+  networking.networkmanager.enable = true;
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  # Enables wireless support via wpa_supplicant.
+  networking.wireless.enable = false;  
+  
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  system.autoUpgrade = {
+    enable = autoUpgrade;
+    dates = "04:00";
+    flake = "${configDir}";
+    flags = [ "--update-input" "nixpkgs" "--commit-lock-file" ];
+    allowReboot = false;
+  };
+
+  time = { timeZone = timeZone; };
+  i18n.defaultLocale = defaultLocale;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = extraLocale;
+    LC_IDENTIFICATION = extraLocale;
+    LC_MEASUREMENT = extraLocale;
+    LC_MONETARY = extraLocale;
+    LC_NAME = extraLocale;
+    LC_NUMERIC = extraLocale;
+    LC_PAPER = extraLocale;
+    LC_TELEPHONE = extraLocale;
+    LC_TIME = extraLocale;
+  };
+
+  services = {
+    xserver = {
+      enable = true;
+      xkb.layout = keyboardLayout;
+      xkb.variant = "";
+    };
+    gnome.gnome-keyring.enable = true;
+    # Helps with Browsers
+    # See: https://wiki.archlinux.org/title/Profile-sync-daemon
+    psd = {
+      enable = true;
+      resyncTimer = "10m";
+    };
+  };
+  console.keyMap = keyboardLayout;
+
+  environment.variables = {
+    XDG_DATA_HOME = "$HOME/.local/share";
+    PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
+    EDITOR = "nvim";
+    TERMINAL = "kitty";
+    TERM = "kitty";
+    BROWSER = "firefox";
+  };
+  
+  # Enable touchpad suppor
+  services.libinput.enable = false;
+  programs.dconf.enable = true;
+  services = {
+    dbus = {
+      enable = true;
+      implementation = "broker";
+      packages = with pkgs; [ gcr gnome-settings-daemon ];
+    };
+    # Allows access network locations (like FTP, SFTP, SMB).
+    gvfs.enable = true;
+    # Switch between: power saving / balanced / performance profiles.
+    power-profiles-daemon.enable = true;
+    # daemon for managing disk drives.
+    udisks2.enable = true;
+  };
+
+  # Faster rebuilding
+  documentation = {
+    enable = true;
+    doc.enable = false;
+    man.enable = true;
+    dev.enable = false;
+    info.enable = false;
+    nixos.enable = false;
+  };
+
+  environment.systemPackages = with pkgs; [
+    hyprland-qtutils
+    fd
+    bc
+    gcc
+    git-ignore
+    xdg-utils
+    wget
+    curl
+    vim
+  ];
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = [ "gtk" ];
+      # hyprland.default = [ "gtk" "hyprland" ];
+    };
+
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
+  security = {
+    # allow wayland lockers to unlock the screen
+    # pam.services.hyprlock.text = "auth include login";
+
+    # userland niceness
+    rtkit.enable = true;
+
+    # don't ask for password for wheel group
+    sudo.wheelNeedsPassword = false;
+  };
+}
