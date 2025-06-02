31d552ba7bdf872673213a059f0d1f877270bc91
LuposX <schuppsimon5@gmail.com>
2025-06-02 14:50:37 +0200

    hyprland bindings

--- nixos/hyprland.nix
+++ nixos/hyprland.nix
@@ -1,6 +1,11 @@
-{ inputs, pkgs, ... }: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   programs.hyprland = {
     enable = true;
+    # Universal Wayland Manager, recommended way to start Hyprland.
     withUWSM = true;
     package = inputs.hyprland.packages."${pkgs.system}".hyprland;
     portalPackage =
