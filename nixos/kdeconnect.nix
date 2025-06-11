21b2845fe3d5194f5dc248366f836bb25ec0dea5
LuposX <schuppsimon5@gmail.com>
2025-06-11 18:07:32 +0200

    changes kdeconnect
    changes kdeconnect
    
    changes kdeconnect
    
    changes kdeconnect

--- nixos/kdeconnect.nix
+++ nixos/kdeconnect.nix
@@ -1,3 +1,16 @@
-{
-  programs.kdeconnect.enable = true;
+{pkgs, ...}: {
+  programs.kdeconnect = {
+    enable = true;
+    # package = pkgs.valent;
+  };
+
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 }
