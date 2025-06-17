8e177abe3f33c4bf36534420e32741b36411872b
LuposX <schuppsimon5@gmail.com>
2025-06-17 18:07:14 +0200

    removed vimium plugin

--- home/programs/firefox/default.nix
+++ home/programs/firefox/default.nix
@@ -72,10 +72,10 @@
         };
 
         # Vimium<D-d>
-        "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
-          install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
-          installation_mode = "force_installed";
-        };
+        # "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+        #   install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+        #   installation_mode = "force_installed";
+        # };
       };
     };
   };
