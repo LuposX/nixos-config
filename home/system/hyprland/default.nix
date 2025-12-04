37cfdf6ab751593c5d8775bcec2079a15f3012e5
LuposX <schuppsimon5@gmail.com>
2025-12-04 12:17:11 +0100

    wallpaper and flake update

--- home/system/hyprland/default.nix
+++ home/system/hyprland/default.nix
@@ -171,6 +171,10 @@ in {
         mfact = 0.5;
       };
 
+      ecosystem = {
+        no_donation_nag = true;
+      };
+
       misc = {
         vfr = true;
         disable_hyprland_logo = true;
@@ -178,7 +182,6 @@ in {
         disable_autoreload = true;
         focus_on_activate = true;
         new_window_takes_over_fullscreen = 2;
-        no_donation_nag = true;
       };
 
       windowrulev2 = [
