218943ef4bc3d782bd482d68376f7a4bb82de287
LuposX <schuppsimon5@gmail.com>
2026-05-06 23:08:56 +0200

    fixes Noctalia error

--- home/system/niri/settings.nix
+++ home/system/niri/settings.nix
@@ -65,7 +65,7 @@
         MOZ_ENABLE_WAYLAND = "1";
         NIXOS_OZONE_WL = "1";
         ANKI_WAYLAND = "1";
-        QT_QPA_PLATFORM = "wayland,xcb";
+        QT_QPA_PLATFORM = "wayland";
         QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         ELECTRON_OZONE_PLATFORM_HINT = "auto";
 
